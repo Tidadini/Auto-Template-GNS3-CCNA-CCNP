6723165d36af3c4588ca471e00c74d393b046d3f
Tidadini <tidadini@algerietelecom.dz>
2019-09-27 22:49:09 +0200

    need interaction for creat LAB

--- script.py
+++ script.py
@@ -2,14 +2,16 @@ import os
 import requests
 import json
 
+# request the GNS3 Server to get projects
 r = requests.get("http://localhost:3080/v2/projects")
 r.json()
 print(r.json())
 
-
+# put the responce from GNS3 Server in JSON file.
 with open("person.json", "w") as json_file:
     json.dump(r.json(), json_file)
 
+# read data from JSON file.
 with open("person.json", "r") as myfile:
     data = myfile.read()
 # parse file
@@ -17,48 +19,52 @@ print("project_ID:  " + obj[0]["project_id"])
 myproject = str(obj[0]["project_id"])
 print(myproject)
 
-
+# send command to GNS3 Server : create VPC1 Node.
 cmd_vpc1 = (
     "curl -i -X POST 'http://localhost:3080/v2/projects/"
     + myproject
     + "/nodes'"
     + ' -d \'{"compute_id": "local", "name": "vpc1", "node_type": "vpcs","x":10,"y":0,"z":10}\''
 )
-print(cmd_vpc1)
 os.system(cmd_vpc1)
+
+# send command to GNS3 Server : create VPC2 Node.
 cmd_vpc2 = (
     "curl -i -X POST 'http://localhost:3080/v2/projects/"
     + myproject
     + "/nodes'"
     + ' -d \'{"compute_id": "local", "name": "vpc2", "node_type": "vpcs","x": 10,"y":100,"z":10}\''
 )
-print(cmd_vpc2)
 os.system(cmd_vpc2)
+
+# send command to GNS3 Server : create VPC3 Node.
 cmd_vpc3 = (
     "curl -i -X POST 'http://localhost:3080/v2/projects/"
     + myproject
     + "/nodes'"
     + ' -d \'{"compute_id": "local", "name": "vpc3", "node_type": "vpcs","x": 10,"y":200,"z":10}\''
 )
-print(cmd_vpc3)
 os.system(cmd_vpc3)
+
+# send command to GNS3 Server : create VPC4 Node.
 cmd_vpc4 = (
     "curl -i -X POST 'http://localhost:3080/v2/projects/"
     + myproject
     + "/nodes'"
     + ' -d \'{"compute_id": "local", "name": "vpc4", "node_type": "vpcs","x": 10,"y":300,"z":10}\''
 )
-print(cmd_vpc4)
 os.system(cmd_vpc4)
+
+# send command to GNS3 Server : create R1 Node.
 cmd_R = (
     "curl http://localhost:3080/v2/projects/"
     + myproject
     + "/nodes"
     + ' -d \'{"symbol": ":/symbols/router.svg", "name": "R1", "properties": {"platform": "c7200", "nvram": 512, "image": "c7200-adventerprisek9-mz.152-4.M7.image", "ram": 512, "slot3": "PA-GE", "system_id": "FTX0945W0MY", "slot0": "C7200-IO-FE", "slot2": "PA-GE","slot4":"PA-GE", "slot1": "PA-GE",  "idlepc": "0x606e0538"}, "compute_id": "local", "node_type": "dynamips","x":-300,"y":150,"z":10}\''
 )
-print(cmd_R)
 os.system(cmd_R)
 
+# request the GNS3 Server to get Nodes IDs
 r1 = requests.get(
     "http://localhost:3080/v2/projects/" + obj[0]["project_id"] + "/nodes"
 )
@@ -79,7 +85,7 @@ print("node_ID:  " + obj[2]["node_id"])
 print("node_ID:  " + obj[3]["node_id"])
 print("node_ID:  " + obj[4]["node_id"])
 
-
+# send command to GNS3 Server : create link between R1--Pc1
 PC1_R1 = (
     'curl -X POST  "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -91,6 +97,8 @@ PC1_R1 = (
     + '", "port_number": 0}]}\''
 )
 os.system(PC1_R1)
+
+# send command to GNS3 Server : create link between R1--Pc2
 PC2_R1 = (
     'curl -X POST  "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -102,6 +110,8 @@ PC2_R1 = (
     + '", "port_number": 0}]}\''
 )
 os.system(PC2_R1)
+
+# send command to GNS3 Server : create link between R1--Pc3
 PC3_R1 = (
     'curl -X POST  "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -113,6 +123,8 @@ PC3_R1 = (
     + '", "port_number": 0}]}\''
 )
 os.system(PC3_R1)
+
+# send command to GNS3 Server : create link between R1--Pc4
 PC4_R1 = (
     'curl -X POST  "http://localhost:3080/v2/projects/'
     + obj[3]["project_id"]
@@ -125,6 +137,7 @@ PC4_R1 = (
 )
 os.system(PC4_R1)
 
+# Start Nodes
 Start_PC1 = (
     'curl -X POST "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -133,6 +146,8 @@ Start_PC1 = (
     + '/start" -d "{}"'
 )
 os.system(Start_PC1)
+
+# Start Nodes
 Start_PC2 = (
     'curl -X POST "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -141,6 +156,8 @@ Start_PC2 = (
     + '/start" -d "{}"'
 )
 os.system(Start_PC2)
+
+# Start Nodes
 Start_PC3 = (
     'curl -X POST "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -149,6 +166,8 @@ Start_PC3 = (
     + '/start" -d "{}"'
 )
 os.system(Start_PC3)
+
+# Start Nodes
 Start_PC4 = (
     'curl -X POST "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
@@ -157,6 +176,8 @@ Start_PC4 = (
     + '/start" -d "{}"'
 )
 os.system(Start_PC4)
+
+# Start Nodes
 Start_R1 = (
     'curl -X POST "http://localhost:3080/v2/projects/'
     + obj[0]["project_id"]
